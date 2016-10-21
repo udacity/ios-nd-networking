a7e1fc025f40e7a40941f0ff0e0bca4492b0ec46
Jarrod Parkes <jarrod@udacity.com>
2016-10-21 09:24:15 -0700

    update starter comment for TMM v1 get and post functions

--- TheMovieManager-v1/TheMovieManager/TMDBClient.swift
+++ TheMovieManager-v1/TheMovieManager/TMDBClient.swift
@@ -33,11 +33,11 @@ class TMDBClient : NSObject {
 
     // MARK: GET
     
-    //func taskForGETMethod(method: String, var parameters: [String:AnyObject], completionHandlerForGET: (result: AnyObject!, error: NSError?) -> Void) -> NSURLSessionDataTask {}
+    //func taskForGETMethod(_ method: String, parameters: [String:AnyObject], completionHandlerForGET: @escaping (_ result: AnyObject?, _ error: NSError?) -> Void) -> URLSessionDataTask {}
     
     // MARK: POST
     
-    //func taskForPOSTMethod(method: String, var parameters: [String:AnyObject], jsonBody: [String:AnyObject], completionHandlerForPOST: (result: AnyObject!, error: NSError?) -> Void) -> NSURLSessionDataTask {}
+    //func taskForPOSTMethod(_ method: String, parameters: [String:AnyObject], jsonBody: String, completionHandlerForPOST: @escaping (_ result: AnyObject?, _ error: NSError?) -> Void) -> URLSessionDataTask {}
     
     // MARK: GET Image
     
