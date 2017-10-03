93acccd2f14738d33da39abdcbdaad87897a1765
Jarrod Parkes <parkesfjarrod@gmail.com>
2017-10-03 05:38:29 -0500

    use commons dog url

--- ImageRequestComplete/ImageRequest/Constants.swift
+++ ImageRequestComplete/ImageRequest/Constants.swift
@@ -10,5 +10,5 @@
 
 struct Constants {
     static let CatURL = "https://upload.wikimedia.org/wikipedia/commons/4/4d/Cat_November_2010-1a.jpg"
-    static let PuppyURL = "http://www.puppiesden.com/pics/1/poodle-puppy2.jpg"
+    static let DogURL = "https://upload.wikimedia.org/wikipedia/commons/e/ec/Terrier_mixed-breed_dog.jpg"
 }
---
+++ ImageRequestMVC/ImageRequest/Image.swift
@@ -0,0 +1,9 @@
+//
+//  Image.swift
+//  ImageRequest
+//
+//  Created by Jarrod Parkes on 10/3/17.
+//  Copyright © 2017 Udacity. All rights reserved.
+//
+
+import Foundation
